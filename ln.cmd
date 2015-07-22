9ab429e1e3e2308eba5d85151407f3ea72f95812
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-07-22 15:34:02 +0900

    ln.js -> ln.cmd and let not make new session to make hardlink or junction.

--- ln.cmd
+++ ln.cmd
@@ -1,3 +1,7 @@
+@if (0) == (0) echo off
+cscript //nologo /E:JScript "%~f0" %*
+exit /b %ERRORLEVEL%
+@end
 var args = WScript.Arguments
 
 if( args.length < 2 ){
@@ -18,7 +22,6 @@ var dst = fsObj.GetAbsolutePathName(args.Item(start+1));
 if( fsObj.FolderExists(dst) ){
     dst = fsObj.BuildPath(dst,fsObj.GetFileName(src));
 }
-var shellApp = new ActiveXObject("Shell.Application");
 var opt=""
 var action="open"
 if( symbolic ){
@@ -30,7 +33,17 @@ if( symbolic ){
     opt = (fsObj.FolderExists(src) ? "/J" : "/H");
 }
 var param="/c mklink "+opt+' "'+dst+'" "'+src+'"';
-shellApp.ShellExecute("CMD.EXE",param,"",action);
-WScript.Echo("CMD.EXE " + param);
+if( symbolic ){
+    var shellApp = new ActiveXObject("Shell.Application");
+    shellApp.ShellExecute("cmd.exe",param,"",action);
+}else{
+    var wshell = new ActiveXObject("WScript.Shell");
+    var oExec = wshell.Exec("cmd.exe " + param);
+    while( oExec.Status == 0 ){
+        WScript.Sleep(100);
+    }
+}
+WScript.Echo("cmd.exe " + param);
 WScript.Echo("    " + src + "\n--> " + dst);
 WScript.Quit(0);
+// vim:set ft=JavaScript:
