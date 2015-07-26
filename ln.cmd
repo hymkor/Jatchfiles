ff216e20b2a0dc567f98e0ae27d1ea5caf363f72
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-07-26 21:29:08 +0900

    ln.cmd supports the wildcard expansion.

--- ln.cmd
+++ ln.cmd
@@ -1,5 +1,9 @@
 @if (0) == (0) echo off
-cscript //nologo /E:JScript "%~f0" %*
+SETLOCAL ENABLEDELAYEDEXPANSION
+set "ARGV="
+for %%I in (%*) do set ARGV=!ARGV! "%%~I"
+cscript //nologo /E:JScript "%~f0" %ARGV%
+endlocal
 exit /b %ERRORLEVEL%
 @end
 var args = WScript.Arguments
@@ -16,27 +20,42 @@ if( args.Item(0) == "-s" ){
     symbolic = true;
     ++start;
 }
-var src = fsObj.GetAbsolutePathName(args.Item(start+0));
-var dst = fsObj.GetAbsolutePathName(args.Item(start+1));
+var src = [];
+for(var i=start ; i < args.length-1 ; i++ ){
+    src.push( fsObj.GetAbsolutePathName(args.Item(start+i)) );
+}
+var dst = fsObj.GetAbsolutePathName(args.Item(args.length-1));
 
-if( fsObj.FolderExists(dst) ){
-    dst = fsObj.BuildPath(dst,fsObj.GetFileName(src));
-}
-var opt=""
-var action="open"
-if( symbolic ){
-    if( fsObj.FolderExists(src) ){
-        opt = "/D";
+function makeparam(src1,dst1){
+    var result="";
+    if( fsObj.FolderExists(dst1) ){
+        result = '"' + fsObj.BuildPath(dst1,fsObj.GetFileName(src1)) + '" ';
+    }else{
+        result = '"' + dst1 + '" ';
     }
-    action = "runas"
-}else{
-    opt = (fsObj.FolderExists(src) ? "/J" : "/H");
+    result += '"' + src1 + '" ';
+    return result;
 }
-var param="/c mklink "+opt+' "'+dst+'" "'+src+'"';
+
+var param="/c ";
 if( symbolic ){
+    for(var i=0 ; i < src.length ; i++ ){
+        if( i > 0 ){
+            param += "& ";
+        }
+        param += (fsObj.FolderExists(src[i]) ? "mklink /D " : "mklink ");
+        param += makeparam(src[i],dst);
+    }
     var shellApp = new ActiveXObject("Shell.Application");
-    shellApp.ShellExecute("cmd.exe",param,"",action);
+    shellApp.ShellExecute("cmd.exe",param,"","runas");
 }else{
+    for(var i=0 ; i < src.length ; i++ ){
+        if( i > 0 ){
+            param += "& ";
+        }
+        param += (fsObj.FolderExists(src[i]) ? "mklink /J " : "mklink /H ");
+        param += makeparam(src[i],dst);
+    }
     var wshell = new ActiveXObject("WScript.Shell");
     var oExec = wshell.Exec("cmd.exe " + param);
     while( oExec.Status == 0 ){
@@ -44,6 +63,5 @@ if( symbolic ){
     }
 }
 WScript.Echo("cmd.exe " + param);
-WScript.Echo("    " + src + "\n--> " + dst);
 WScript.Quit(0);
 // vim:set ft=JavaScript:
